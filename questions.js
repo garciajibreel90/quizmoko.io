d3cae0071e1e6cc7e896ab8a8e529ab6d83a6dc1
garciajibreel90 <jibreelgarcia45@gmail.com>
2024-05-10 07:28:13 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "It is the deterioration of rocks and minerals.",
+        answer: "d. Weathering",
+        options: [
+            "a. Erosion",
+            "b. Photosynthesis",
+            "c. Protein synthesis",
+            "d. Weathering"
+        ]
+    },
+    {
+        numb: 2, 
+        question: "This scientist formulated the seafloor spreading theory.",
+        answer: "b. Harry Hess",
+        options: [
+            "a. Alfred Wegener",
+            "b. Harry Hess",
+            "c. Charles Darwin",
+            "d. Ferdinand Marcos Jr."
+        ]
+    } ,
+    {
+        numb: 3, 
+        question: "Seismology is the study of?",
+        answer: "b. earthquakes",
+        options: [
+            "a. volcanoes",
+            "b. earthquakes",
+            "c. life of Leni Robredo",
+            "d. The earth composition"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "Volcanology is a branch of earth science, but what is it about?",
+        answer: "a. volcanoes",
+        options: [
+            "a. volcanoes",
+            "b. earthquakes",
+            "c. life",
+            "d. the weather"
+
+        ]
+    },
+    {
+        numb: 5, 
+        question: "Before the seafloor spreading theory was formulated, there was this theory, a theory referring to the continents moving away from eachother.",
+        answer: "a. Continental drift theory",
+        options: [
+            "a. Continental drift theory",
+            "b. Theory of Relativity",
+            "c. Theory of Biological Evolution by Natural Selection",
+            "d. Bigbang Theory"
+        ]
+    }
+]
